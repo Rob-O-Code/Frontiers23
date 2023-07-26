2b128573c57a7d17389350d3f415875ae2946b66
Oleynick <rpoleynick@wpi.edu>
2023-07-26 14:13:53 -0400

    Finish numgame

--- 01-numgame/numgame.js
+++ 01-numgame/numgame.js
@@ -7,9 +7,19 @@ let secret;
 function loadGame() {
     guess.max = MAXNUM;
     secret = Math.floor(Math.random() * (MAXNUM+1));
-    report.innerHTML = secret;
+    // report.innerHTML = secret;
 }
 
 function makeGuess() {
+    let myGuess = guess.value;
 
+    if (myGuess < secret) {
+        report.innerHTML += `<br/>[${myGuess}] too small`;
+        // console.log(`myGuess = ${myGuess}`);
+    } else if (myGuess > secret) {
+        report.innerHTML += `<br/>[${myGuess}] too large`;
+    } else {
+        report.innerHTML += `<br/>[${myGuess}] is correct! :)`;
+        // YOU WIN (DO SOMETHING HERE)
+    }
 }
