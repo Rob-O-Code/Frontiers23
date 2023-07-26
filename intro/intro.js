3d6d09dd8f3b5256fc53c9ac8f34f42f4a501e07
Oleynick <rpoleynick@wpi.edu>
2023-07-26 09:29:19 -0400

    Working intro

--- intro/intro.js
+++ intro/intro.js
@@ -1,3 +1,8 @@
 function clickButton() {
     console.log(`Hello world!`);
+    const mytext = document.getElementById("mytext");
+    mytext.innerHTML = `It worked!`;
+    
+    const body = document.body;
+    body.style = `background-color: red`;
 }
