8cd0455dca5d8aac3c93fa0331e68c7e2192792b
Oleynick <rpoleynick@wpi.edu>
2023-07-26 10:03:34 -0400

    Add input box

--- intro/intro.js
+++ intro/intro.js
@@ -7,6 +7,8 @@ function clickButton() {
     const mytext2 = document.getElementById("mytext2");
     mytext2.className = `myclass`;
     
+    const bgcolor = document.getElementById("bgcolor");
+
     const body = document.body;
-    body.style.backgroundColor = `#ddddff`;
+    body.style.backgroundColor = bgcolor.value;
 }
