5b28b2715b5a466c9159a7d2ab86fb054d4f9cdd
Oleynick <rpoleynick@wpi.edu>
2023-07-26 09:49:53 -0400

    Update intro

--- intro/intro.js
+++ intro/intro.js
@@ -2,7 +2,11 @@ function clickButton() {
     console.log(`Hello world!`);
     const mytext = document.getElementById("mytext");
     mytext.innerHTML = `It worked!`;
+    mytext.className = `myclass`;
+
+    const mytext2 = document.getElementById("mytext2");
+    mytext2.className = `myclass`;
     
     const body = document.body;
-    body.style = `background-color: red`;
+    body.style.backgroundColor = `#ddddff`;
 }
